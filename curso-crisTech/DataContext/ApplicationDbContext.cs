9e455d0ba79a67eed9d2e13ae418a1012cff921f
Thiago Souza <tsouzatm@outlook.com>
2024-01-25 11:16:34 -0300

    Configuração DbContext e primeira migration

---
+++ curso-crisTech/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using curso_crisTech.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace curso_crisTech.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+        }
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+    }
+}
